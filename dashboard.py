9af8fd9eb5d17c7f4217c8635ec856fa0392d1df
umairahmad-co <umairahmad.co@gmail.com>
2026-06-23 20:28:35 +0500

    initial

--- dashboard.py
+++ dashboard.py
@@ -9,10 +9,10 @@ window.resizable(0,0)
 window.configure(bg="white")
 
 bg_image= PhotoImage(file="logo.png")
-title_label = Label(window,image=bg_image,compound=LEFT, text="Inventory Manegment System", fg="white", bg="#010c48",font=("times new roman",40,'bold'))
-title_label.place(x=0,y=0)
+title_label = Label(window,image=bg_image,compound=RIGHT, text="Inventory Manegment System", fg="white", bg="#010c48",anchor='w',padx=20,font=("times new roman",40,'bold'))
+title_label.place(x=0,y=0,relwidth=1)
 
-
+logoutButton = Button(window,text="Logout",command=window.quit)
 
 window.mainloop()
 
